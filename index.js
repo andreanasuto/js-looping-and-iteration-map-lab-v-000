6dbef4d36aee8b06c1e26b2d619b6987005d3f87
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-02 17:29:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function lowerCaseDrivers(drivers) {
   const lowercasedrivers = drivers.map(function (name) {
   const newarray = []
   newarray.push(name.toLowerCase())
-  
+
   })
-  return newarray
+  
 }
