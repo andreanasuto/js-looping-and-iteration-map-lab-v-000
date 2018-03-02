5827e5cf10058c859c9c924ea94e77b682cfd1fd
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-02 17:31:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
   const lowercasedrivers = drivers.map(function (name) {
-  const newarray = []
-  newarray.push(name.toLowerCase())
-  console.log(newarray)
+  return Object.assign([], name, name.toLowerCase())
   })
-  
+
 }
