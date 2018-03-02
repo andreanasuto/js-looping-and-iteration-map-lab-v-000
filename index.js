57a258f33d8c4bd1190f6d77fcd3c9c70d196402
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-02 17:15:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-  drivers.filter(function (name) {
-    name.toLowerCase()
+  const lowerCaseDrivers = drivers.map(function (name) {
+   name.toLowerCase()
   })
 }
